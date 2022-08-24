433ead1b24aa8e8b1a7f8aa6429a95fee6f9af16
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-08-25 00:25:38 +0600

    night working

--- src/jsx/pages/homepage/Homepage.js
+++ src/jsx/pages/homepage/Homepage.js
@@ -4,6 +4,8 @@ function Homepage(props) {
     return (
         <div id='Homepage'>
             <Section1></Section1>
+            <Section2></Section2>
+            <Section3></Section3>
         </div>
     );
 }
@@ -22,7 +24,51 @@ const Section1 = () => {
         </p>
 
         <button className="btn_know">
-            KNOW MORE!
+            <img src={require('../../../assets/homepage/know_more.png')}
+                alt="img" />
         </button>
+    </section>
+}
+
+const Section2 = () => {
+    return (
+        <section id='Section2'>
+            <div className="section_title">
+                <img src={require('../../../assets/homepage/section_title_bit.png')} alt="img" />
+            </div>
+            <div className="section_body">
+                <div className="video_frame">
+                    <iframe
+                        width={560}
+                        height={315}
+                        src="https://www.youtube.com/embed/p3z-VAwRTtM"
+                        title="YouTube video player"
+                        frameBorder={0}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen=""
+                    />
+                </div>
+                <div className="content_wrapper">
+                    <p className='paragraph'>
+                        Bit Royale is the most promising NFT game of 2022!
+                        Here, you’ll collect NFT heroes and assemble a team strong enough to defeat other players or bosses and take back the control of the Bit Royal Kingdom from a tyrant and cruel king!
+                        <br /><br />
+                        To play the game use your “BCOMP” tokens to purchase your NFT heroes, trainers and lands from our P2E game.
+                    </p>
+                    <button className='btn_more'>
+                        <span>
+                            MORE ABOUT
+                            BIT ROYALE
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+const Section3 = () => {
+    return <section id="Section3">
+
     </section>
 }
